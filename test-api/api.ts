ecea886276b8a4adb8848d82a35b6306053c3d96
Harsh Karki <corecorecrorecrore@gmail.com>
2025-04-09 18:03:13 +0100

    decouple rate-limiter-method & test-api

---
+++ test-api/api.ts
@@ -0,0 +1,15 @@
+import * as http from "http";
+import { IncomingMessage, ServerResponse } from "http";
+import { rateLimiter } from "../rate-limiter/custom-rate-limit";
+
+const server: http.Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    if (!rateLimiter(req, res)) return; // Apply Rate Limiter First
+
+    // Actual API logic (runs only if not rate-limited)
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello, API Response!");
+  }
+);
+
+server.listen(3000, () => console.log("Server running on port 3000"));
