e0beb482d7451f29a595367297fee2583efe1b62
Harsh Karki <corecorecrorecrore@gmail.com>
2025-04-21 18:00:29 +0100

    play aroudn with setting and retrieving values from redis.

--- src/initialize/index.ts
+++ src/initialize/index.ts
@@ -1,12 +1,30 @@
 // index.ts or main entry point
 import { connectRedis } from '../redis/index.js';
-import { crud } from '../redis/crud-operations.js';
+import { crud ,addCounter} from '../redis/crud-operations.js';
 // however you start your service
 
+const INTERVAL_TIME = 10; // try changing this value and check logs
 (async () => {
   try {
     await connectRedis(); // connect first
-    await crud(); 
+    console.log("-------------")
+    console.log("creating coutner key in redis with 5 seconds ttl")
+    await addCounter('counter', 5); // example usage
+
+   const intervalId = setInterval(async () => {
+   let num  : number = await addCounter('counter', 5);
+   console.log("Counter incremented successfully! after .5 seconds", num);
+
+   if(num>5){ 
+      console.log("Counter value is greater than 5, stopping incrementing.");
+      clearInterval(intervalId); 
+      return;
+   }else { 
+      console.log("Counter value is less than 5, incrementing again.");
+   }
+}, INTERVAL_TIME);
+
+
   } catch (err) {
     console.error('🚨 Could not start service:', err);
     process.exit(1);
