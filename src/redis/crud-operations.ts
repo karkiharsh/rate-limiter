af004e2c272ba0c0b7bb18fca9f984bcfb5f40ad
Harsh Karki <corecorecrorecrore@gmail.com>
2025-04-21 17:12:53 +0100

    Add addCounter Atomic expression using lua script

--- src/redis/crud-operations.ts
+++ src/redis/crud-operations.ts
@@ -1,7 +1,21 @@
 import { client } from "./index.js";
 
 
+const addCounter = async (key: string, ttl: number) => {
+  const script = `
+    local current = redis.call('INCR', KEYS[1])
+    if current == 1 then
+      redis.call('EXPIRE', KEYS[1], ARGV[1])
+    end
+    return current
+  `; // lua scripts are evalulated as atomic operations to counter race-conditions
+  const result = await client.eval(script, {
+    keys: [key],
+    arguments: [ttl.toString()],
+  });
 
+  console.log("Counter incremented successfully!", result);
+};
 const  crud = async ()=> { 
     console.log(" Trying crud operations....")
     try {
@@ -16,4 +30,4 @@ const  crud = async ()=> {
         process.exit(1);
       }
 }
-export { crud };
+export { crud , addCounter };
