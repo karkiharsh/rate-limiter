8ec10db4f8eb2e5b8d1ebde30cb6593958b61ace
Harsh Karki <corecorecrorecrore@gmail.com>
2025-04-22 15:05:52 +0100

    use configurable WINDOW_SIZE and MAX_REQUESTS constants

--- src/initialize/index.ts
+++ src/initialize/index.ts
@@ -1,6 +1,7 @@
 // index.ts or main entry point
 import { connectRedis } from '../redis/index.js';
 import { crud ,addCounter} from '../redis/crud-operations.js';
+import { WINDOW_SIZE, MAX_REQUESTS } from '../config/rate-limiting-rules/config.js';
 // however you start your service
 
 const INTERVAL_TIME = 10; // try changing this value and check logs
@@ -9,14 +10,14 @@ const INTERVAL_TIME = 10; // try changing this value and check logs
     await connectRedis(); // connect first
     console.log("-------------")
     console.log("creating coutner key in redis with 5 seconds ttl")
-    await addCounter('counter', 5); // example usage
+    await addCounter('counter', WINDOW_SIZE); // example usage
 
    const intervalId = setInterval(async () => {
    let num  : number = await addCounter('counter', 5);
    console.log("Counter incremented successfully! after .5 seconds", num);
 
-   if(num>5){ 
-      console.log("Counter value is greater than 5, stopping incrementing.");
+   if(num>MAX_REQUESTS){ 
+      console.log(`Counter value is greater than ${MAX_REQUESTS}, stopping incrementing.`);
       clearInterval(intervalId); 
       return;
    }else { 
