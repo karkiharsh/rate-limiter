42732204bedf1b26cb99e6e48a692c1f3ddc787f
Harsh Karki <corecorecrorecrore@gmail.com>
2025-04-22 16:44:55 +0100

    Refactor initialization function to improve readability and structure

--- src/initialize/index.ts
+++ src/initialize/index.ts
@@ -5,29 +5,14 @@ import { WINDOW_SIZE, MAX_REQUESTS } from '../config/rate-limiting-rules/config.
 // however you start your service
 
 const INTERVAL_TIME = 10; // try changing this value and check logs
-(async () => {
+const initialize = async () => {
   try {
+    console.log('Establishing connection to Redis...');
     await connectRedis(); // connect first
-    console.log("-------------")
-    console.log("creating coutner key in redis with 5 seconds ttl")
-    await addCounter('counter', WINDOW_SIZE); // example usage
-
-   const intervalId = setInterval(async () => {
-   let num  : number = await addCounter('counter', 5);
-   console.log("Counter incremented successfully! after .5 seconds", num);
-
-   if(num>MAX_REQUESTS){ 
-      console.log(`Counter value is greater than ${MAX_REQUESTS}, stopping incrementing.`);
-      clearInterval(intervalId); 
-      return;
-   }else { 
-      console.log("Counter value is less than 5, incrementing again.");
-   }
-}, INTERVAL_TIME);
-
 
   } catch (err) {
     console.error('🚨 Could not start service:', err);
     process.exit(1);
   }
-})();
+};
+initialize(); 
