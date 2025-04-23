3b7ec5001e1f182a331b5dcdacb5426991c439fa
Harsh Karki <corecorecrorecrore@gmail.com>
2025-04-23 15:35:04 +0100

    A simple initialization structure

--- src/initialize/index.ts
+++ src/initialize/index.ts
@@ -1,14 +1,28 @@
 // index.ts or main entry point
 import { connectRedis } from '../redis/index.js';
 import { crud ,addCounter} from '../redis/crud-operations.js';
-import { WINDOW_SIZE, MAX_REQUESTS } from '../config/rate-limiting-rules/config.js';
+import { WINDOW_SIZE, MAX_REQUESTS } from '../config/rate-limiting.js';
+import { port } from '../config/local-server.js'; // import your port from env-validator or config
+import { startServer } from '../orchestrate/server.js';
 // however you start your service
 
 const INTERVAL_TIME = 10; // try changing this value and check logs
 const initialize = async () => {
   try {
     console.log('Establishing connection to Redis...');
-    await connectRedis(); // connect first
+
+    let intervalId = setInterval(() => {console.log('.'); },400);
+
+    await connectRedis();
+    clearInterval(intervalId);
+
+    console.log('Spinning up the server...');
+
+    intervalId = setInterval(() => {console.log('.')  },100);
+
+    await startServer(); 
+    clearInterval(intervalId);
+    console.log('---');      
 
   } catch (err) {
     console.error('🚨 Could not start service:', err);
