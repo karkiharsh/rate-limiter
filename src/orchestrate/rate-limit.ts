1c021d9c0d745c9b223ff624e0327c3748a19349
Harsh Karki <corecorecrorecrore@gmail.com>
2025-04-23 17:06:30 +0100

    add basic rateLimiter method

---
+++ src/orchestrate/rate-limit.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { addCounter } from "../redis/crud-operations.js";
+import { WINDOW_SIZE, MAX_REQUESTS } from "../config/rate-limiting.js";
+
+const rateLimit = async (key:string) => {
+
+  const hitCount: number = await addCounter(key, WINDOW_SIZE);
+  if(hitCount > MAX_REQUESTS) {
+    return false; // rate limit exceeded
+  }
+  return true; // within rate limit
+}
+
+export {rateLimit}; 
