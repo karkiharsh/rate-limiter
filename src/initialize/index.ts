f2b1375bb21cb723f546ab05eec9ab312c200340
Harsh Karki <corecorecrorecrore@gmail.com>
2025-04-23 17:09:39 +0100

    formatting changes  +  remove unused references

--- src/initialize/index.ts
+++ src/initialize/index.ts
@@ -1,24 +1,21 @@
 // index.ts or main entry point
 import { connectRedis } from '../redis/index.js';
-import { crud ,addCounter} from '../redis/crud-operations.js';
 import { WINDOW_SIZE, MAX_REQUESTS } from '../config/rate-limiting.js';
 import { port } from '../config/local-server.js'; // import your port from env-validator or config
-import { startServer } from '../orchestrate/server.js';
+import { startServer } from '../orchestrate/start-server.js';
 // however you start your service
 
 const INTERVAL_TIME = 10; // try changing this value and check logs
 const initialize = async () => {
   try {
     console.log('Establishing connection to Redis...');
-
     let intervalId = setInterval(() => {console.log('.'); },400);
 
     await connectRedis();
     clearInterval(intervalId);
 
     console.log('Spinning up the server...');
-
-    intervalId = setInterval(() => {console.log('.')  },100);
+    intervalId = setInterval(() => {console.log('.')  },400);
 
     await startServer(); 
     clearInterval(intervalId);
