fed488ef113409df5855b5acef462a2f541a5ac5
Harsh Karki <corecorecrorecrore@gmail.com>
2025-07-28 18:03:55 +0530

    add a new clean redisClient

---
+++ src/config/redisClient.ts
@@ -0,0 +1,13 @@
+// src/config/redisClient.ts
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient.on('error', (err) => console.error('Redis error', err));
+
+await redisClient.connect();
