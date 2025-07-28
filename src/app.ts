259ee5c4d766d2f031e09832a53c5fdc342132b9
Harsh Karki <corecorecrorecrore@gmail.com>
2025-07-28 18:51:04 +0530

    add health routes to express application instance

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 // src/app.ts
 import express from 'express';
 import clientRoutes from './routes/clientRoutes';
-// import adminRoutes from './routes/adminRoutes';
+import adminRoutes from './routes/adminRoutes';
+import healthRoutes from './routes/ping';
 import  errorHandler  from './middlewares/errorHandler';
-
 const app = express();
 app.use(express.json());
 app.use('/api', clientRoutes);
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
+app.use('/healthRoutes', healthRoutes);
 app.use(errorHandler);
-
 export default app;
