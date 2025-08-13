9bf6cb98ec9977b7e709ac9c93de6cebb46dcd4a
Harsh Karki <corecorecrorecrore@gmail.com>
2025-08-13 18:23:55 +0530

    moint pingRouter at /ping

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 // src/app.ts
 import express from 'express';
+import pingRouter from './routes/ping.js';
 // import clientRoutes from './routes/clientRoutes.js';
 // import adminRoutes from './routes/adminRoutes.js';
 // import healthRoutes from './routes/ping.js';
 // import  errorHandler  from './middlewares/errorHandler.js';
 const app = express();
+app.use('/ping',pingRouter); 
 // app.use(express.json());
 // app.use('/api', clientRoutes);
 // app.use('/admin', adminRoutes);
