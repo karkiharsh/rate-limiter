3f7d2eb99eafa8425f38bd57fd3fb6642c7c833c
Harsh Karki <corecorecrorecrore@gmail.com>
2025-09-02 22:10:10 +0530

    mount clientRouter

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 // src/app.ts
-import express from 'express';
-import pingRouter from './routes/ping.js';
+import express from "express";
+import pingRouter from "./routes/ping.js";
+import clientRouter from "./routes/clientRoutes.js";
 // import clientRoutes from './routes/clientRoutes.js';
 // import adminRoutes from './routes/adminRoutes.js';
 // import healthRoutes from './routes/ping.js';
 // import  errorHandler  from './middlewares/errorHandler.js';
 const app = express();
 app.use(express.json());
-app.use('/ping',pingRouter); 
+app.use("/ping", pingRouter);
+app.use("/client", clientRouter);
 // app.use(express.json());
 // app.use('/api', clientRoutes);
 // app.use('/admin', adminRoutes);
